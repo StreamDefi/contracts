dcd13395a4d8237a95d031aa751542aee7670448
Alejandro Gala <alegala21@gmail.com>
2023-12-12 15:53:13 -0500

    init

---
+++ src/lib/Vault.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.4;
+
+/*
+  * @title Vault
+  * @dev Vault Data Type library for Stream Vaults
+  */
+*/
+library Vault {
+    struct VaultParams {
+        // Token decimals for vault shares
+        uint8 decimals;
+        // Asset used in Stream Vault
+        address asset;
+        // Minimum supply of the vault shares issued, for ETH it's 10**10
+        uint56 minimumSupply;
+        // Vault cap
+        uint104 cap;
+    }
+
+    struct VaultState {
+        // 32 byte slot 1
+        //  Current round number. `round` represents the number of `period`s elapsed.
+        uint16 round;
+        // Amount that is currently locked for executing strategy
+        uint104 lockedAmount;
+        // Amount that was locked for executing strategy in the previous round
+        // used for calculating performance fee deduction
+        uint104 lastLockedAmount;
+        // 32 byte slot 2
+        // Stores the total tally of how much of `asset` there is
+        // to be used to mint rSTREAM tokens
+        uint128 totalPending;
+        // Total amount of queued withdrawal shares from previous rounds (doesn't include the current round)
+        uint128 queuedWithdrawShares;
+    }
+
+    struct DepositReceipt {
+        // Maximum of 65535 rounds. Assuming 1 round is 7 days, maximum is 1256 years.
+        uint16 round;
+        // Deposit amount, max 20,282,409,603,651 or 20 trillion ETH deposit
+        uint104 amount;
+        // Unredeemed shares balance
+        uint128 unredeemedShares;
+    }
+
+    struct Withdrawal {
+        // Maximum of 65535 rounds. Assuming 1 round is 7 days, maximum is 1256 years.
+        uint16 round;
+        // Number of shares withdrawn
+        uint128 shares;
+    }
+}
