146ed7c3517a5692d83086695d026ba82df1f83c
Alejandro Gala <alegala21@gmail.com>
2024-01-31 23:43:53 -0500

    natspec

--- src/VaultKeeper.sol
+++ src/VaultKeeper.sol
@@ -6,11 +6,29 @@ import {StreamVault} from "./StreamVault.sol";
 import {Vault} from "./lib/Vault.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+/*
+ * @title - VaultKeeper
+ * @notice - This contract is responsible for rolling rounds and managing vaults
+ * @notice - This contract takes the place of the Keeper in StreamVault to avoid front-runs
+ */
 contract VaultKeeper is Ownable {
+    /************************************************
+     *  STATE
+     ***********************************************/
+
     mapping(string => address) public vaults;
 
     constructor() Ownable(msg.sender) {}
 
+    /************************************************
+     *  ROLLING ROUND
+     ***********************************************/
+
+    /**
+     * @notice - Roll round for a list of vaults. Vaults should be added to state before rolling round
+     * @param tickers - List of vault tickers
+     * @param lockedBalances - List of locked balances
+     */
     function rollRound(
         string[] calldata tickers,
         uint256[] calldata lockedBalances
@@ -20,9 +38,39 @@ contract VaultKeeper is Ownable {
             "VaultKeeper: Invalid input"
         );
         for (uint256 i = 0; i < tickers.length; i++) {
+            require(
+                vaults[tickers[i]] != address(0),
+                "VaultKeeper: Invalid vault"
+            );
             _rollRound(tickers[i], lockedBalances[i]);
         }
     }
+
+    /************************************************
+     *  MANAGEMENT
+     ***********************************************/
+    function addVault(
+        string calldata ticker,
+        address vault
+    ) external onlyOwner {
+        vaults[ticker] = vault;
+    }
+
+    function removeVault(string calldata ticker) external onlyOwner {
+        delete vaults[ticker];
+    }
+
+    function withdraw(address token, uint256 amount) external onlyOwner {
+        if (token == address(0)) {
+            payable(owner()).transfer(amount);
+        } else {
+            ERC20(token).transfer(owner(), amount);
+        }
+    }
+
+    /************************************************
+     *  HELPERS
+     ***********************************************/
 
     function _rollRound(string memory ticker, uint256 lockedBalance) internal {
         StreamVault vault = StreamVault(payable(vaults[ticker]));
@@ -50,24 +98,5 @@ contract VaultKeeper is Ownable {
             "VaultKeeper: Not enough assets"
         );
         return queuedWithdrawShares;
-    }
-
-    function addVault(
-        string calldata ticker,
-        address vault
-    ) external onlyOwner {
-        vaults[ticker] = vault;
-    }
-
-    function removeVault(string calldata ticker) external onlyOwner {
-        delete vaults[ticker];
-    }
-
-    function withdraw(address token, uint256 amount) external onlyOwner {
-        if (token == address(0)) {
-            payable(owner()).transfer(amount);
-        } else {
-            ERC20(token).transfer(owner(), amount);
-        }
     }
 }
