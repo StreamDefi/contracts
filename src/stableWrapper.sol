8b9365b67dc00430b964565b0bfae34e7f830c78
0xDro <alegala21@gmail.com>
2025-01-25 15:49:11 -0800

    rename contract to StableWrapper

--- src/stableWrapper.sol
+++ src/stableWrapper.sol
@@ -7,7 +7,7 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract SimpleVault is ERC20, Ownable, ReentrancyGuard {
+contract StableWrapper is ERC20, Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     // State variables
